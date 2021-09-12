1a4e3ea26ac14e79b55a2f54ff036dd094ac1138
Leslie Wen <leslie.wen@gmail.com>
2021-09-12 12:51:36 -0400

    added boilerplate ver 0.00.02

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+def window():
+    app=QApplication(sys.argv) #know what OS to Run
+    win=QMainWindow()
+    win.setGeometry(200,200,700,700)
+    win.setWindowTitle("SimpleTextEditor")
+    win.show()
+    sys.exit(app.exec_())
+
+window()
